cf1914bdcb4c312564bb506ba6edac2d040a1063
farid95g <faridg.1995@gmail.com>
2022-08-15 14:53:37 +0400

    connecting to mongodb database

---
+++ mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the all the arguments: node mongo.js <password> <name> <phoneNumber>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://faridg1995:${password}@cluster0.nw3lfgb.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+    id: Number,
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+mongoose
+    .connect(url)
+    .then(() => {
+        if (!name && !number) {
+            console.log('phonebook:')
+            return Person.find({}).then((people) => {
+                if (people.length) {
+                    people.forEach(person => {
+                        console.log(`${person.name} ${person.number}`)
+                    })
+                } else {
+                    console.log('No entries yet.')
+                }
+            })
+        }
+        
+        const newPerson = new Person({
+            id: Math.random(),
+            name,
+            number
+        })
+
+        return newPerson.save().then(() => {
+            console.log(`Added ${name} number ${number} to phonebook.`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+    .finally(() => mongoose.connection.close())
